007c577e50b92b7dba339202de90ef73c3c7c755
Kfir Strikovsky <kfirs@wix.com>
2026-01-25 14:26:47 +0200

    small fix

--- src/cli/utils/runCommand.ts
+++ src/cli/utils/runCommand.ts
@@ -5,7 +5,6 @@ import { CLIExitError } from "../errors.js";
 import { printBanner } from "./banner.js";
 import { login } from "../commands/auth/login.js";
 import { theme } from "./theme.js";
-import { CLIExitError } from "../program.js";
 
 export interface RunCommandOptions {
   /**
